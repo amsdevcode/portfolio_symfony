e637b627e72b499d9bb263dee7f5f7aac0943ee8
Amandine <amandinebensekhri@MacBook-Air-de-Amandine.local>
2021-03-26 15:55:25 +0100

    26 mars 15h55

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use Faker;
+use App\Entity\About;
+use App\Entity\Skill;
 use App\Entity\Projet;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -14,27 +16,35 @@ class AppFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
 
-        $projet = new Projet();
-        $projet->setName('Ce site : Symfony 5');
-        $projet->setDescription('Site vitrine - portfolio avec back-office et outils entièrement créés par mes soins');
-        $projet->setImg1('projet1.png');
-        $manager->persist($projet);
-
-        $faker = Faker\Factory::create();
-
-        for ($i = 1; $i <= 10; $i++) {
+        /*
             $projet = new Projet();
-            $projet->setName('Projet de ' . $faker->name());
-            $projet->setDescription($faker->text(255));
+            $projet->setName('Ce site : Symfony 5');
+            $projet->setDescription('Site vitrine - portfolio avec back-office et outils entièrement créés par mes soins');
             $projet->setImg1('projet1.png');
             $manager->persist($projet);
-        }
-        $manager->flush();
 
-        $skill = new Skill();
-        $skill->setCategory('Technologies');
-        $skill->setLogo('logo-html.png');
-        $skill->setTitle('HTML');
-        $manager->persist($skill);
+            $faker = Faker\Factory::create();
+
+            for ($i = 1; $i <= 10; $i++) {
+                $projet = new Projet();
+                $projet->setName('Projet de ' . $faker->name());
+                $projet->setDescription($faker->text(255));
+                $projet->setImg1('projet' . $i . '.png');
+                $manager->persist($projet);
+            }
+
+            $skill = new Skill();
+            $skill->setCategory('Technologies');
+            $skill->setLogo('logo-html.png');
+            $skill->setTitle('HTML');
+            $manager->persist($skill);
+
+            $about = new About();
+            $about->setTitle('À propos');
+            $about->setDescription('À propos de moi');
+            $manager->persist($about);
+        */
+
+        $manager->flush();
     }
 }
